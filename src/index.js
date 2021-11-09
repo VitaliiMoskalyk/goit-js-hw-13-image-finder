a627abbceb2adf9f5850a1e74d979ae66a434b03
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2021-11-09 22:59:04 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './sass/main.scss';
 import { getImages } from './js/apiService';
-// import { debounce, times } from 'lodash';
 import imagesList from './templates/image_temp.hbs';
 import * as basicLightbox from 'basiclightbox'
+////////////////////////////////////////////////////////////////////////////////////
 
 const searchData = {
     key: '24190237-c75eaa2cb0fd0521e8d3d1887',
@@ -15,59 +15,54 @@ const queryselectors = {
     ingListRef: document.querySelector('.gallery'),
     button_moreload: document.querySelector('.button_moreload'),
     upBtn: document.querySelector('.button_upper'),
-    svg: document.querySelector('.svg'),
+    svg: document.querySelector('.main-animation'),
     inputArea:document.querySelector('.input-area'),
 };
 
+/////////////////////////////////////////////////////////////////////////////////////
+
 queryselectors.searchEl.addEventListener('click', getFormTextContent);
 queryselectors.inputArea.addEventListener('keydown', (e) => {
-  
- 
-      
-        
-    
-    
     if (e.keyCode === 13) {
-      event.preventDefault();
+      e.preventDefault();
         window.history.back();
         getFormTextContent()
     }
 });
+
 queryselectors.button_moreload.addEventListener('click', overLoad);
 queryselectors.upBtn.addEventListener('click', () => upperBtn(element2));
 
 function getFormTextContent() {
     searchData.pageNumber = 1;
     searchData.searchValue = queryselectors.searchEl.previousElementSibling.value;
+    queryselectors.ingListRef.innerHTML = ``;
     
-    const API = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchData.searchValue}&page=${searchData.pageNumber}&per_page=12&key=${searchData.key}`;
-    
-    queryselectors.ingListRef.innerHTML=``;
-    getImages(API).then(o => {
+    getImages(searchData.searchValue,searchData.pageNumber,searchData.key).then(o => {
         markupBeforeend(o.hits);
         if (o.hits.length >= 1) queryselectors.svg.classList.add('visually-hidden');
         if (o.hits.length > 11) {
             queryselectors.button_moreload.classList.remove('visually-hidden');
             queryselectors.upBtn.classList.remove('visually-hidden');
         }
-    })
-        
+    })      
 }
 
 function overLoad() {
     searchData.pageNumber = searchData.pageNumber + 1;
-   
-    const API = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchData.searchValue}&page=${searchData.pageNumber}&per_page=12&key=${searchData.key}`;
-
-    getImages(API).then(o => markupBeforeend(o.hits))
-        .then(()=>upperBtn(element)
+ 
+    getImages(searchData.searchValue, searchData.pageNumber, searchData.key).then(o => markupBeforeend(o.hits))
+        .then(() => upperBtn(element)
 )}
 
-function markupBeforeend(data) {
+function markupBeforeend(data) { 
     if (data.length > 0) {
             const imagesMarkup = createMarkup(data, imagesList);
             queryselectors.ingListRef.insertAdjacentHTML('beforeend', imagesMarkup);
-        }
+    }
+    else {
+            queryselectors.button_moreload.classList.add('visually-hidden');
+    }
 }
 
 
@@ -77,7 +72,7 @@ const element2 = document.getElementById('search-form');
 function upperBtn(el) { 
   el.scrollIntoView({
   behavior: 'smooth',
-  block: 'end',
+  block: 'start',
 });
 }
 
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,18 +1,15 @@
 
-
-
-export const getImages = async (api) => {
-    
+export const getImages = async (Value,Number,key) => {
+    const API = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${Value}&page=${Number}&per_page=12&key=${key}`;
     try {
-        const response = await fetch(api);
-        console.log(response.ok);
+        const response = await fetch(API);
         const responseData = response.json();
-        return responseData;
+            return responseData;
+
     } catch (err) {
         console.log(response.err)
         throw err;
     }
-    
 }
 
 
